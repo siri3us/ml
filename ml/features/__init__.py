8b54e173a95b632970b93883ed798961d153d3c9
siri3us <siri3us@yandex.ru>
2017-11-24 16:34:46 +0300

    Deleting files

--- ml/features/__init__.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-
-from .feature_base import FeatureBase
-from .categorical_feature import CategoricalFeature
-from .numerical_feature import NumericalFeature
-# from .features_storage import FeaturesStorage
-# from .categorical_combiner import CategoricalCombiner
-# from .aggregated_feature import AggregatedFeature
