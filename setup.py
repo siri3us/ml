ccd45e0c89184ddf357ed4bd89304bc9162f4624
siri3us <siri3us@yandex.ru>
2017-11-24 15:55:00 +0300

    setup.py added for the first commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+# setup.py
+from distutils.core import setup
+
+setup(name='ml',
+      version='1.0',
+      author='siri3us', 
+      author_email='siri3us@yandex.ru',
+      url='http://github.com/siri3us/ml',
+      packages=['ml', 'ml.features', 'ml.mixtures', 'ml.utils', 'ml.core'],
+     )
