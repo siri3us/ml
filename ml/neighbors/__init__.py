2b32278a74a14d198b1e6b21ae5859ea7c015ef1
siri3us <siri3us@yandex.ru>
2017-11-27 21:05:06 +0300

    Modified

--- ml/neighbors/__init__.py
+++ ml/neighbors/__init__.py
@@ -1,1 +1,2 @@
 # -*- coding: utf-8 -*-
+from .k_nearest_neighbor import KNearestNeighbor
--- ml/utils/__init__.py
+++ ml/utils/__init__.py
@@ -1,1 +1,4 @@
 # -*- coding: utf-8 -*-
+from .data_utils import get_CIFAR10_data, load_CIFAR10
+from .vis_utils import *
+from .gradient_check import *
